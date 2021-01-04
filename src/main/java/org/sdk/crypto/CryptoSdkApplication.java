e22ae8cdc5d263b24ce06b33b0c142d5d0cbc1f8
zhangzhenwei <zzhangzhenwei@bjca.org.cn>
2021-01-04 16:29:30 +0800

    提交基础工程

---
+++ src/main/java/org/sdk/crypto/CryptoSdkApplication.java
@@ -0,0 +1,13 @@
+package org.sdk.crypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoSdkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoSdkApplication.class, args);
+	}
+
+}
