2d0bb11230e8f2ad23b938a29d9fca3c873c56d5
zhenwei1108 <zhenwei1108@163.com>
2022-01-27 22:26:31 +0800

    add provider to builder

--- sdk/src/main/java/com/github/zhenwei/sdk/key/KeyPairBuilder.java
+++ sdk/src/main/java/com/github/zhenwei/sdk/key/KeyPairBuilder.java
@@ -2,27 +2,31 @@ package com.github.zhenwei.sdk.key;
 
 import com.github.zhenwei.core.asn1.gm.GMNamedCurves;
 import com.github.zhenwei.core.asn1.gm.GMObjectIdentifiers;
-import com.github.zhenwei.provider.jce.provider.ChaosProvider;
 import com.github.zhenwei.sdk.enums.KeyAlgEnum;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
+import java.security.Provider;
 import java.security.SecureRandom;
 import java.security.spec.ECGenParameterSpec;
 
 public class KeyPairBuilder {
 
+  private Provider provider;
 
+  public KeyPairBuilder(Provider provider) {
+    this.provider = provider;
+  }
 
-  public static void build(KeyAlgEnum keyAlgEnum)
+  public void build(KeyAlgEnum keyAlgEnum)
       throws NoSuchAlgorithmException, InvalidAlgorithmParameterException, NoSuchProviderException {
     //SM2 算法曲线
     String name = GMNamedCurves.getName(GMObjectIdentifiers.sm2p256v1);
     ECGenParameterSpec sm2Spec = new ECGenParameterSpec(name);
 
-    KeyPairGenerator generator = KeyPairGenerator.getInstance("EC", new ChaosProvider());
+    KeyPairGenerator generator = KeyPairGenerator.getInstance("EC", provider);
     generator.initialize(sm2Spec,new SecureRandom());
     KeyPair keyPair = generator.generateKeyPair();
     System.out.println(keyPair);
