ebe63a6f39daee77e18055040d6469c3b30c9f35
zhenwei1108 <zhenwei1108@163.com>
2022-01-31 23:31:44 +0800

    merge code

--- sdk/src/test/java/com/github/zhenwei/test/key/KeyPairTest.java
+++ sdk/src/test/java/com/github/zhenwei/test/key/KeyPairTest.java
@@ -3,7 +3,7 @@ package com.github.zhenwei.test.key;
 import com.github.zhenwei.provider.jce.provider.WeGooProvider;
 import com.github.zhenwei.sdk.enums.KeyAlgEnum;
 import com.github.zhenwei.sdk.exception.BaseWeGooException;
-import com.github.zhenwei.sdk.key.KeyPairBuilder;
+import com.github.zhenwei.sdk.key.KeyBuilder;
 import com.github.zhenwei.sdk.util.Base64Util;
 import java.security.KeyPair;
 import org.junit.Test;
@@ -12,7 +12,7 @@ public class KeyPairTest {
 
   @Test
   public void genRsa1024Key() throws BaseWeGooException {
-    KeyPairBuilder builder = new KeyPairBuilder(new WeGooProvider());
+    KeyBuilder builder = new KeyBuilder(new WeGooProvider());
     KeyPair keyPair = builder.build(KeyAlgEnum.RSA_1024);
     System.out.println(Base64Util.encode(keyPair.getPrivate().getEncoded()));
     System.out.println(Base64Util.encode(keyPair.getPublic().getEncoded()));
@@ -22,7 +22,7 @@ public class KeyPairTest {
 
   @Test
   public void genSM2Key() throws BaseWeGooException {
-    KeyPairBuilder builder = new KeyPairBuilder(new WeGooProvider());
+    KeyBuilder builder = new KeyBuilder(new WeGooProvider());
     KeyPair keyPair = builder.build(KeyAlgEnum.SM2_256);
     System.out.println(Base64Util.encode(keyPair.getPrivate().getEncoded()));
     System.out.println(Base64Util.encode(keyPair.getPublic().getEncoded()));
