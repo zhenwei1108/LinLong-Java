2782491b9c4c7eda65164d6d8b21a6ae39a89873
zhenwei1108 <zhenwei1108@163.com>
2022-02-14 19:25:32 +0800

    完善加解密操作,填充iv

--- sdk/src/main/java/com/github/zhenwei/sdk/builder/CipherBuilder.java
+++ sdk/src/main/java/com/github/zhenwei/sdk/builder/CipherBuilder.java
@@ -6,6 +6,7 @@ import com.github.zhenwei.sdk.exception.WeGooCipherException;
 import java.security.Key;
 import java.security.Provider;
 import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
 
 public class CipherBuilder {
 
@@ -15,12 +16,23 @@ public class CipherBuilder {
     this.provider = provider;
   }
 
-  public byte[] cipher(CipherAlgEnum cipherAlgEnum, Key key, byte[] sourceData, boolean encrypt)
+  public byte[] cipher(CipherAlgEnum cipherAlgEnum, Key key, byte[] sourceData,
+      IvParameterSpec ivParameterSpec, boolean encrypt)
       throws WeGooCipherException {
     try {
       Cipher cipher = Cipher.getInstance(cipherAlgEnum.getAlg(), provider);
-      cipher.init(encrypt ? Cipher.ENCRYPT_MODE : Cipher.DECRYPT_MODE, key);
+      if (cipherAlgEnum.isNeedIv()) {
+        if (ivParameterSpec == null) {
+          throw new WeGooCipherException(CipherExceptionMessageEnum.iv_param_empty_err);
+        }
+
+        cipher.init(encrypt ? Cipher.ENCRYPT_MODE : Cipher.DECRYPT_MODE, key, ivParameterSpec);
+      } else {
+        cipher.init(encrypt ? Cipher.ENCRYPT_MODE : Cipher.DECRYPT_MODE, key);
+      }
       return cipher.doFinal(sourceData);
+    } catch (WeGooCipherException e) {
+      throw e;
     } catch (Exception e) {
       throw new WeGooCipherException(CipherExceptionMessageEnum.cipher_data_err, e);
     }
--- sdk/src/main/java/com/github/zhenwei/sdk/enums/exception/CipherExceptionMessageEnum.java
+++ sdk/src/main/java/com/github/zhenwei/sdk/enums/exception/CipherExceptionMessageEnum.java
@@ -4,6 +4,7 @@ public enum CipherExceptionMessageEnum implements IExceptionEnum {
   cipher_data_err("cipher data error","秘密处理失败"),
   encrypt_data_err("encrypt data error","加密失败"),
   decrypt_data_err("decrypt data error","解密失败"),
+  iv_param_empty_err("iv param empty error","初始化向量 iv 为空"),
   ;
 
   private String message;
