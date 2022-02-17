5c356147e8d795a483735f7bd633d49638b7e70c
zhenwei1108 <zhenwei1108@163.com>
2022-02-17 22:30:46 +0800

    完善mac支持

--- sdk/src/main/java/com/github/zhenwei/sdk/builder/Mac.java
+++ sdk/src/main/java/com/github/zhenwei/sdk/builder/Mac.java
@@ -1,14 +1,43 @@
 package com.github.zhenwei.sdk.builder;
 
+import com.github.zhenwei.core.crypto.Digest;
+import com.github.zhenwei.core.crypto.digests.SHA1Digest;
+import com.github.zhenwei.core.crypto.digests.SHA224Digest;
+import com.github.zhenwei.core.crypto.digests.SHA256Digest;
+import com.github.zhenwei.core.crypto.digests.SHA384Digest;
+import com.github.zhenwei.core.crypto.digests.SHA512Digest;
+import com.github.zhenwei.core.crypto.digests.SM3Digest;
+import com.github.zhenwei.core.crypto.macs.HMac;
+import com.github.zhenwei.core.crypto.params.KeyParameter;
+import com.github.zhenwei.sdk.enums.DigestAlgEnum;
+import com.github.zhenwei.sdk.enums.exception.CryptoExceptionMassageEnum;
+import com.github.zhenwei.sdk.exception.WeGooCryptoException;
+import java.security.Key;
+
 public class Mac {
 
 
-  public void hmac(){
-
+  public byte[] hmac(DigestAlgEnum digestAlgEnum, Key key, byte[] source) {
+    Digest digest;
+    switch (digestAlgEnum) {
+      case SHA1: digest = new SHA1Digest(); break;
+      case SHA224: digest = new SHA224Digest(); break;
+      case SHA256: digest = new SHA256Digest(); break;
+      case SHA384: digest = new SHA384Digest(); break;
+      case SHA512: digest = new SHA512Digest(); break;
+      //default sm3 digest
+      default: digest = new SM3Digest(); break;
+    }
+    HMac hMac = new HMac(digest);
+    hMac.init(new KeyParameter(key.getEncoded()));
+    hMac.update(source, 0, source.length);
+    byte[] result = new byte[hMac.getMacSize()];
+    hMac.doFinal(result, 0);
+    return result;
   }
 
-  public void cmac(){
-
+  public void cmac() throws WeGooCryptoException {
+    throw new WeGooCryptoException(CryptoExceptionMassageEnum.not_support_now);
   }
 
 }
