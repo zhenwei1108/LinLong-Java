81eb993d1cd3e1cb006585be1126ad5d93fb01a1
zhangzhenwei <zhenwei1108@163.com>
2022-02-20 23:05:12 +0800

    补充P10构造

--- sdk/src/main/java/com/github/zhenwei/sdk/builder/P10Builder.java
+++ sdk/src/main/java/com/github/zhenwei/sdk/builder/P10Builder.java
@@ -1,6 +1,47 @@
 package com.github.zhenwei.sdk.builder;
 
+import com.github.zhenwei.pkix.pkcs.PKCS10CertificationRequest;
+import com.github.zhenwei.sdk.enums.exception.CryptoExceptionMassageEnum;
+import com.github.zhenwei.sdk.exception.WeGooCryptoException;
+import com.github.zhenwei.sdk.util.Base64Util;
+
+import java.io.IOException;
+import java.security.PublicKey;
+import java.security.Signature;
+import java.util.List;
+
 public class P10Builder {
 
+    private PKCS10CertificationRequest request;
+
+    public P10Builder(byte[] data) throws WeGooCryptoException {
+        try {
+            request = new PKCS10CertificationRequest(data);
+        } catch (Exception e) {
+            throw new WeGooCryptoException(CryptoExceptionMassageEnum.parse_p10_err);
+        }
+    }
+
+    public String getSubjectDN() throws IOException {
+        return request.getSubject().toString();
+    }
+
+    public PublicKey getPublicKey() {
+        return (PublicKey) request.getSubjectPublicKeyInfo();
+    }
+
+    public String getP10() throws WeGooCryptoException {
+        try {
+            return Base64Util.encode(request.getEncoded());
+        } catch (Exception e) {
+            throw new WeGooCryptoException(CryptoExceptionMassageEnum.encode_err, e);
+        }
+    }
+
+
+    public P10Builder(String dn, PublicKey publicKey, Signature signature, List<Object> list){
+
+
+    }
 
 }
--- sdk/src/main/java/com/github/zhenwei/sdk/enums/exception/CryptoExceptionMassageEnum.java
+++ sdk/src/main/java/com/github/zhenwei/sdk/enums/exception/CryptoExceptionMassageEnum.java
@@ -3,7 +3,9 @@ package com.github.zhenwei.sdk.enums.exception;
 public enum CryptoExceptionMassageEnum implements IExceptionEnum {
   params_short_err("params is too short", "参数太短"),
   parse_crl_err("parse crl error", "解析CRL失败"),
+  parse_p10_err("parse p10 error", "解析P10失败"),
   build_err("build error", "构造失败"),
+  encode_err("encode error", "编码失败"),
 
   ;
 
