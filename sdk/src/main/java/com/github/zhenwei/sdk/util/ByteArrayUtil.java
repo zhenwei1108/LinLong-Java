3581a94b926800c787e7077ca153701428d5c1c8
zhangzhenwei <zhenwei1108@163.com>
2022-02-23 19:47:13 +0800

    补充工具

---
+++ sdk/src/main/java/com/github/zhenwei/sdk/util/ByteArrayUtil.java
@@ -0,0 +1,19 @@
+package com.github.zhenwei.sdk.util;
+
+import java.util.Arrays;
+
+public class ByteArrayUtil {
+
+    public void mergeBytes(byte[]... data) {
+        if (data != null && data.length > 0) {
+            Arrays.stream(data).forEach(bytes->{
+
+            });
+
+
+        }
+
+    }
+
+
+}
