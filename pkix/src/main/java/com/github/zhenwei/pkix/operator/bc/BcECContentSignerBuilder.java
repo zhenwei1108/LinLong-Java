7db949bc4db9e2c798bae802a7ee6ca8e49a82a8
zhangzhenwei <zhenwei1108@163.com>
2022-02-24 22:21:21 +0800

    组装p10适配SM3WithSM2

--- pkix/src/main/java/com/github/zhenwei/pkix/operator/bc/BcECContentSignerBuilder.java
+++ pkix/src/main/java/com/github/zhenwei/pkix/operator/bc/BcECContentSignerBuilder.java
@@ -1,10 +1,13 @@
 package com.github.zhenwei.pkix.operator.bc;
 
+import com.github.zhenwei.core.asn1.gm.GMObjectIdentifiers;
 import com.github.zhenwei.core.asn1.x509.AlgorithmIdentifier;
 import com.github.zhenwei.core.crypto.Digest;
 import com.github.zhenwei.core.crypto.Signer;
+import com.github.zhenwei.core.crypto.digests.SM3Digest;
 import com.github.zhenwei.core.crypto.signers.DSADigestSigner;
 import com.github.zhenwei.core.crypto.signers.ECDSASigner;
+import com.github.zhenwei.core.crypto.signers.SM2Signer;
 import com.github.zhenwei.pkix.operator.OperatorCreationException;
 
 public class BcECContentSignerBuilder
@@ -17,7 +20,10 @@ public class BcECContentSignerBuilder
   protected Signer createSigner(AlgorithmIdentifier sigAlgId, AlgorithmIdentifier digAlgId)
       throws OperatorCreationException {
     Digest dig = digestProvider.get(digAlgId);
-
+    //SM3WithSM2 算法使用固定 UID, 公钥参与摘要运算
+    if (dig instanceof SM3Digest && sigAlgId.getAlgorithm().equals(GMObjectIdentifiers.sm2sign_with_sm3)){
+      return new SM2Signer(dig);
+    }
     return new DSADigestSigner(new ECDSASigner(), dig);
   }
 }
