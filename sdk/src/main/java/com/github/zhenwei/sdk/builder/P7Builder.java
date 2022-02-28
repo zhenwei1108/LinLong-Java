15d368fbf89068efb9d074cd05e4e6a58219bebc
zhenwei1108 <zhenwei1108@163.com>
2022-02-28 22:49:55 +0800

    下一步规划，完善时间戳

--- sdk/src/main/java/com/github/zhenwei/sdk/builder/P7Builder.java
+++ sdk/src/main/java/com/github/zhenwei/sdk/builder/P7Builder.java
@@ -3,7 +3,8 @@ package com.github.zhenwei.sdk.builder;
 /**
  * @description: P7Builder
  *  pkcs 7 构造者， 包含： 签名，信封
- *  参考 RFC-2315
+ *  参考 RFC-2315(p7) RFC-3852(CMS)
+ *
  * @author: zhangzhenwei
  * @since: 1.0.0
  * @date: 2022/2/28  10:32 下午
---
+++ sdk/src/main/java/com/github/zhenwei/sdk/builder/TimeStampBuilder.java
@@ -0,0 +1,12 @@
+package com.github.zhenwei.sdk.builder;
+
+/**
+ * @description: TimeStamp
+ *  时间戳 RFC-3161
+ * @author: zhangzhenwei
+ * @since: 1.0.0
+ * @date: 2022/2/28  10:48 下午
+ */
+public class TimeStampBuilder {
+
+}
