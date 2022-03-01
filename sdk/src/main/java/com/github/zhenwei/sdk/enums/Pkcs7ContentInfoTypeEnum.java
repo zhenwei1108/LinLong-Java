71cb4a619887a263fa76b0d1172079ae9100e2f7
zhenwei1108 <zhenwei1108@163.com>
2022-03-01 19:13:58 +0800

    补充p7类型枚举

---
+++ sdk/src/main/java/com/github/zhenwei/sdk/enums/Pkcs7ContentInfoTypeEnum.java
@@ -0,0 +1,28 @@
+package com.github.zhenwei.sdk.enums;
+
+import com.github.zhenwei.core.asn1.ASN1ObjectIdentifier;
+import com.github.zhenwei.core.asn1.pkcs.PKCSObjectIdentifiers;
+
+/**
+ * @description: Pkcs7ContentInfoTypeEnum
+ *  PKCS7 类型枚举，参考 RFC-2315
+ *   ContentType: data, signedData, envelopedData, signedAndEnvelopedData, digestedData, and encryptedData
+ * @author: zhangzhenwei
+ * @since: 1.0.0
+ * @date: 2022/3/1  7:08 下午
+ */
+public enum Pkcs7ContentInfoTypeEnum {
+    DATA(PKCSObjectIdentifiers.data),
+    SIGNED_DATA(PKCSObjectIdentifiers.signedData),
+    ENVELOPED_DATA(PKCSObjectIdentifiers.envelopedData),
+    SIGNED_AND_ENVELOPED_DATA(PKCSObjectIdentifiers.signedAndEnvelopedData),
+    DIGESTED_DATA(PKCSObjectIdentifiers.digestedData),
+    ENCRYPTED_DATA(PKCSObjectIdentifiers.encryptedData),
+    ;
+
+    private ASN1ObjectIdentifier oid;
+
+    Pkcs7ContentInfoTypeEnum(ASN1ObjectIdentifier oid) {
+        this.oid = oid;
+    }
+}
