e632f45d145826d129a5e949f7b74c3a7dcd9c83
zhenwei1108 <zhenwei1108@163.com>
2022-03-02 22:27:10 +0800

    完善P7构造类型

--- sdk/src/main/java/com/github/zhenwei/sdk/builder/P7Builder.java
+++ sdk/src/main/java/com/github/zhenwei/sdk/builder/P7Builder.java
@@ -3,7 +3,10 @@ package com.github.zhenwei.sdk.builder;
 import com.github.zhenwei.core.asn1.ASN1Encodable;
 import com.github.zhenwei.core.asn1.DEROctetString;
 import com.github.zhenwei.core.asn1.pkcs.ContentInfo;
+import com.github.zhenwei.sdk.enums.BasePkcs7TypeEnum;
+import com.github.zhenwei.sdk.enums.GmPkcs7ContentInfoTypeEnum;
 import com.github.zhenwei.sdk.enums.Pkcs7ContentInfoTypeEnum;
+import com.github.zhenwei.sdk.exception.WeGooCipherException;
 
 /**
  * @description: P7Builder
@@ -24,27 +27,71 @@ public class P7Builder {
      * @return com.github.zhenwei.core.asn1.pkcs.ContentInfo
      * @author zhangzhenwei
      * @description 封装pkcs7
-     *  ContentInfo ::= SEQUENCE {
-     *      contentType ContentType,
-     *      content
-     *        [0] EXPLICIT ANY DEFINED BY contentType OPTIONAL }
+     * 参考 RFC-2315 / GMT-0010
+     * ContentInfo ::= SEQUENCE {
+     * contentType ContentType,
+     * content
+     * [0] EXPLICIT ANY DEFINED BY contentType OPTIONAL }
      * @date 2022/3/1  7:27 下午
      * @since: 1.0.0
      */
-    public ContentInfo build(Pkcs7ContentInfoTypeEnum typeEnum, byte[] data) {
+    public ContentInfo build(BasePkcs7TypeEnum typeEnum, byte[] data) throws WeGooCipherException {
+        ContentInfo contentInfo;
+        //根据枚举类型判断是否为国密类型。国密类型OID 有另定义
+        if (typeEnum instanceof Pkcs7ContentInfoTypeEnum) {
+            Pkcs7ContentInfoTypeEnum infoTypeEnum = (Pkcs7ContentInfoTypeEnum) typeEnum;
+            contentInfo = genPkcs7ContentInfo(infoTypeEnum, data);
+        } else if (typeEnum instanceof GmPkcs7ContentInfoTypeEnum) {
+            GmPkcs7ContentInfoTypeEnum infoTypeEnum = (GmPkcs7ContentInfoTypeEnum) typeEnum;
+            contentInfo = genGmPkcs7ContentInfo(infoTypeEnum, data);
+        } else {
+            throw new WeGooCipherException("");
+        }
+        return contentInfo;
+
+
+    }
+
+
+    private ContentInfo genPkcs7ContentInfo(Pkcs7ContentInfoTypeEnum infoTypeEnum, byte[] data) {
         ASN1Encodable asn1Encodable = null;
-        switch (typeEnum) {
+        switch (infoTypeEnum) {
             case DATA:
                 asn1Encodable = genData(data);
                 break;
             case SIGNED_DATA:
+                break;
             case DIGESTED_DATA:
+                break;
             case ENCRYPTED_DATA:
+                break;
             case ENVELOPED_DATA:
+                break;
             case SIGNED_AND_ENVELOPED_DATA:
+                break;
         }
+        return new ContentInfo(infoTypeEnum.getOid(), asn1Encodable);
+    }
 
-        return new ContentInfo(typeEnum.getOid(), asn1Encodable);
+
+    private ContentInfo genGmPkcs7ContentInfo(GmPkcs7ContentInfoTypeEnum infoTypeEnum, byte[] data) {
+        ASN1Encodable asn1Encodable = null;
+        switch (infoTypeEnum) {
+            case DATA:
+                asn1Encodable = genData(data);
+                break;
+            case SIGNED_DATA:
+                break;
+            case KEY_AGREEMENT_INFO_DATA:
+                break;
+            case ENCRYPTED_DATA:
+                break;
+            case ENVELOPED_DATA:
+                break;
+            case SIGNED_AND_ENVELOPED_DATA:
+                break;
+        }
+        return new ContentInfo(infoTypeEnum.getOid(), asn1Encodable);
     }
 
 
--- sdk/src/main/java/com/github/zhenwei/sdk/enums/BasePkcs7TypeEnum.java
+++ sdk/src/main/java/com/github/zhenwei/sdk/enums/BasePkcs7TypeEnum.java
@@ -1,4 +1,7 @@
 package com.github.zhenwei.sdk.enums;
 
-public interface BasePkcs7TypeEnum extends BaseEnum{
+import com.github.zhenwei.core.asn1.ASN1ObjectIdentifier;
+
+public interface BasePkcs7TypeEnum extends BaseEnum {
+    ASN1ObjectIdentifier getOid();
 }
