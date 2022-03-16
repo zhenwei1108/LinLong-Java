44c69bce3d84478aff0b55af57001ae1a04373a3
zhangzhenwei <zhenwei1108@163.com>
2022-03-16 21:35:25 +0800

    证书实现，异常调整

--- sdk/src/main/java/com/github/zhenwei/sdk/builder/CertBuilder.java
+++ sdk/src/main/java/com/github/zhenwei/sdk/builder/CertBuilder.java
@@ -11,7 +11,6 @@ import com.github.zhenwei.core.crypto.params.RSAKeyParameters;
 import com.github.zhenwei.core.util.encoders.Hex;
 import com.github.zhenwei.pkix.cert.X509CertificateHolder;
 import com.github.zhenwei.pkix.cert.X509v3CertificateBuilder;
-import com.github.zhenwei.pkix.operator.OperatorCreationException;
 import com.github.zhenwei.pkix.operator.bc.BcContentSignerBuilder;
 import com.github.zhenwei.pkix.operator.bc.BcECContentSignerBuilder;
 import com.github.zhenwei.pkix.operator.bc.BcRSAContentSignerBuilder;
@@ -27,7 +26,6 @@ import com.github.zhenwei.sdk.exception.WeGooCryptoException;
 import com.github.zhenwei.sdk.util.ByteArrayUtil;
 import com.github.zhenwei.sdk.util.DateUtil;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
@@ -89,43 +87,46 @@ public class CertBuilder {
      * @date 2022/3/15  9:09 下午
      * @since: 1.0.0
      */
-    public static Certificate generateCertificate(String dn, PublicKey publicKey, PrivateKey privateKey) throws OperatorCreationException, IOException, WeGooCryptoException {
-        SubjectPublicKeyInfo keyInfo = SubjectPublicKeyInfo.getInstance(publicKey.getEncoded());
-        X500Name issuer = new X500Name(dn);
-        X500Name subject = new X500Name(dn);
-        byte[] random = RandomBuilder.genRandom(12);
-        byte[] certSn = ByteArrayUtil.mergeBytes("123".getBytes(StandardCharsets.UTF_8), random);
-        BigInteger sn = new BigInteger(certSn);
-        X509v3CertificateBuilder builder = new X509v3CertificateBuilder(issuer, sn, DateUtil.now(), DateUtil.nowPlusDays(360), subject,keyInfo);
+    public static Certificate generateCertificate(String dn, PublicKey publicKey, PrivateKey privateKey) throws WeGooCryptoException {
+        try {
+            SubjectPublicKeyInfo keyInfo = SubjectPublicKeyInfo.getInstance(publicKey.getEncoded());
+            X500Name issuer = new X500Name(dn);
+            X500Name subject = new X500Name(dn);
+            byte[] random = RandomBuilder.genRandom(12);
+            byte[] certSn = ByteArrayUtil.mergeBytes("123".getBytes(StandardCharsets.UTF_8), random);
+            BigInteger sn = new BigInteger(certSn);
+            X509v3CertificateBuilder builder = new X509v3CertificateBuilder(issuer, sn, DateUtil.now(), DateUtil.nowPlusDays(360), subject, keyInfo);
 
-        String algorithm = publicKey.getAlgorithm();
-        BcContentSignerBuilder signerBuilder;
-        AsymmetricKeyParameter parameter;
-        if (algorithm.equals(KeyPairAlgEnum.SM2_256.getAlg())) {
-            AlgorithmIdentifier signAlg = new AlgorithmIdentifier(SignAlgEnum.SM3_WITH_SM2.getOid());
-            AlgorithmIdentifier digAlg = new AlgorithmIdentifier(DigestAlgEnum.SM3.getOid());
-            signerBuilder = new BcECContentSignerBuilder(signAlg, digAlg);
-            ECPrivateKey key = (ECPrivateKey) privateKey;
-            ECParameterSpec parameters = key.getParameters();
-            ECDomainParameters ecDomainParameters = new ECDomainParameters(parameters.getCurve(), parameters.getG(),
-                    parameters.getN(), parameters.getH(), parameters.getSeed());
-            parameter = new ECPrivateKeyParameters(key.getD(), ecDomainParameters);
-        } else {
-            AlgorithmIdentifier signAlg = new AlgorithmIdentifier(SignAlgEnum.SHA256_WITH_RSA.getOid());
-            AlgorithmIdentifier digAlg = new AlgorithmIdentifier(DigestAlgEnum.SHA256.getOid());
-            signerBuilder = new BcRSAContentSignerBuilder(signAlg, digAlg);
-            BCRSAPrivateKey key = (BCRSAPrivateKey) privateKey;
-            parameter = new RSAKeyParameters(true, key.getModulus(), key.getPrivateExponent());
+            String algorithm = publicKey.getAlgorithm();
+            BcContentSignerBuilder signerBuilder;
+            AsymmetricKeyParameter parameter;
+            if (algorithm.equals(KeyPairAlgEnum.SM2_256.getAlg())) {
+                AlgorithmIdentifier signAlg = new AlgorithmIdentifier(SignAlgEnum.SM3_WITH_SM2.getOid());
+                AlgorithmIdentifier digAlg = new AlgorithmIdentifier(DigestAlgEnum.SM3.getOid());
+                signerBuilder = new BcECContentSignerBuilder(signAlg, digAlg);
+                ECPrivateKey key = (ECPrivateKey) privateKey;
+                ECParameterSpec parameters = key.getParameters();
+                ECDomainParameters ecDomainParameters = new ECDomainParameters(parameters.getCurve(), parameters.getG(),
+                        parameters.getN(), parameters.getH(), parameters.getSeed());
+                parameter = new ECPrivateKeyParameters(key.getD(), ecDomainParameters);
+            } else {
+                AlgorithmIdentifier signAlg = new AlgorithmIdentifier(SignAlgEnum.SHA256_WITH_RSA.getOid());
+                AlgorithmIdentifier digAlg = new AlgorithmIdentifier(DigestAlgEnum.SHA256.getOid());
+                signerBuilder = new BcRSAContentSignerBuilder(signAlg, digAlg);
+                BCRSAPrivateKey key = (BCRSAPrivateKey) privateKey;
+                parameter = new RSAKeyParameters(true, key.getModulus(), key.getPrivateExponent());
 
+            }
+
+            X509CertificateHolder build = builder.build(signerBuilder.build(parameter));
+            com.github.zhenwei.core.asn1.x509.Certificate certificate = build.toASN1Structure();
+            return CertBuilder.getInstance(certificate.getEncoded()).getCert();
+        } catch (WeGooCryptoException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new WeGooCryptoException(CryptoExceptionMassageEnum.generate_cert_err, e);
         }
-
-        X509CertificateHolder build = builder.build(signerBuilder.build(parameter));
-        com.github.zhenwei.core.asn1.x509.Certificate certificate = build.toASN1Structure();
-        return CertBuilder.getInstance(certificate.getEncoded()).getCert();
     }
-
-
-
 
 
     public Date getNotBefore() {
