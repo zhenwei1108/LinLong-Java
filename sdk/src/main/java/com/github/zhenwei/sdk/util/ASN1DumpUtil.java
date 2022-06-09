39e579039837a804220437598155a5b9ff0a4730
zhenwei <46163072+zhenwei1108@users.noreply.github.com>
2022-06-09 19:56:56 +0800

    完善dump

--- sdk/src/main/java/com/github/zhenwei/sdk/util/ASN1DumpUtil.java
+++ sdk/src/main/java/com/github/zhenwei/sdk/util/ASN1DumpUtil.java
@@ -7,16 +7,19 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.nio.charset.StandardCharsets;
 
-public class ASN1Dump {
+public class ASN1DumpUtil {
 
-    public static void printDump(String filePath, String targetPath) throws Exception {
+    public static String printDump(String filePath, String targetPath) throws Exception {
         File file = new File(filePath);
         FileInputStream fileInputStream = new FileInputStream(file);
         ASN1InputStream asn1InputStream = new ASN1InputStream(fileInputStream);
         String string = com.github.zhenwei.core.asn1.util.ASN1Dump.dumpAsString(asn1InputStream.readObject(), true);
-        File file1 = new File(targetPath);
-        FileOutputStream stream = new FileOutputStream(file1);
-        stream.write(string.getBytes(StandardCharsets.UTF_8));
+        if (StringUtils.notEmpty(targetPath)){
+            File file1 = new File(targetPath);
+            FileOutputStream stream = new FileOutputStream(file1);
+            stream.write(string.getBytes(StandardCharsets.UTF_8));
+        }
+       return string;
 
     }
 
