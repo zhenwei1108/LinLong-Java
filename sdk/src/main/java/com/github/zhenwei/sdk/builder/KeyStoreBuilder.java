1e5232c2127f259682b6a8fb9731d6a2d876cc6b
zhenwei <46163072+zhenwei1108@users.noreply.github.com>
2022-06-20 22:49:49 +0800

    完善keystore构造的密钥传参

--- sdk/src/main/java/com/github/zhenwei/sdk/builder/KeyStoreBuilder.java
+++ sdk/src/main/java/com/github/zhenwei/sdk/builder/KeyStoreBuilder.java
@@ -12,19 +12,19 @@ import java.security.cert.Certificate;
 
 public class KeyStoreBuilder {
 
-    public byte[] genJks(PrivateKey privateKey, String alias, String passWd, Certificate[] certChain) throws WeGooCryptoException {
-        return gen("jks", privateKey, alias, passWd, certChain);
+    public byte[] genJks(PrivateKey privateKey, String alias, String keyPassWd, String storePassWd, Certificate[] certChain) throws WeGooCryptoException {
+        return generator("jks", privateKey, alias, keyPassWd, storePassWd, certChain);
     }
 
     public KeyStore parseJks(String passWd, byte[] jks) throws WeGooCryptoException {
-        return parse("jks", passWd, jks);
+        return parser("jks", passWd, jks);
     }
 
-    public byte[] genP12(PrivateKey privateKey, String alias, String passWd, Certificate[] certChain) throws WeGooCryptoException {
-        return gen("PKCS12", privateKey, alias, passWd, certChain);
+    public byte[] genP12(PrivateKey privateKey, String alias, String keyPassWd, String storePassWd, Certificate[] certChain) throws WeGooCryptoException {
+        return generator("PKCS12", privateKey, alias, keyPassWd, storePassWd, certChain);
     }
 
-    public KeyStore parse(String type, String passWd, byte[] jks) throws WeGooCryptoException {
+    public KeyStore parser(String type, String passWd, byte[] jks) throws WeGooCryptoException {
         try {
             KeyStore store = KeyStore.getInstance(type, new WeGooProvider());
             ByteArrayInputStream stream = new ByteArrayInputStream(jks);
@@ -35,13 +35,13 @@ public class KeyStoreBuilder {
         }
     }
 
-    public byte[] gen(String type, PrivateKey privateKey, String alias, String passWd, Certificate[] certChain) throws WeGooCryptoException {
+    public byte[] generator(String type, PrivateKey privateKey, String alias, String keyPassWd, String storePassWd, Certificate[] certChain) throws WeGooCryptoException {
         try {
             KeyStore store = KeyStore.getInstance(type, new WeGooProvider());
             store.load(null);
-            store.setKeyEntry(alias, privateKey, passWd.toCharArray(), certChain);
+            store.setKeyEntry(alias, privateKey, keyPassWd.toCharArray(), certChain);
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            store.store(outputStream, passWd.toCharArray());
+            store.store(outputStream, storePassWd.toCharArray());
             return outputStream.toByteArray();
         } catch (Exception e) {
             throw new WeGooCryptoException(CryptoExceptionMassageEnum.generate_jks_err, e);
