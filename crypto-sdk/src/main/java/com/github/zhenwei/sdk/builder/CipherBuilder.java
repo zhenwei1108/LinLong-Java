701dc81da2dd0b85a7a71b87cc35e71ee23b00b1
zhenwei1108 <zhenwei1108@163.com>
2022-07-25 23:13:54 +0800

    完善加密算法实现

--- crypto-sdk/src/main/java/com/github/zhenwei/sdk/builder/CipherBuilder.java
+++ crypto-sdk/src/main/java/com/github/zhenwei/sdk/builder/CipherBuilder.java
@@ -4,6 +4,7 @@ import com.github.zhenwei.core.enums.CipherAlgEnum;
 import com.github.zhenwei.core.enums.exception.CipherExceptionMessageEnum;
 import com.github.zhenwei.core.exception.WeGooCipherException;
 import com.github.zhenwei.sdk.init.ProviderEngine;
+import com.github.zhenwei.sdk.util.BytesUtil;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
@@ -12,30 +13,31 @@ import java.security.Key;
 /**
  * @description: 加解密实现
  * @author: zhangzhenwei
- * @since 1.0.0
  * @date: 2022/2/16 22:41
+ * @since 1.0.0
  */
 public class CipherBuilder {
 
-  public static byte[] cipher(CipherAlgEnum cipherAlgEnum, Key key, byte[] sourceData,
-      IvParameterSpec ivParameterSpec, boolean encrypt) throws WeGooCipherException {
-    try {
-      Cipher cipher = Cipher.getInstance(cipherAlgEnum.getAlg(), ProviderEngine.getProvider());
-      //如果
-      if (cipherAlgEnum.getModeEnum().isNeedIV()) {
-        if (ivParameterSpec == null) {
-          throw new WeGooCipherException(CipherExceptionMessageEnum.iv_param_empty_err);
+    public static byte[] cipher(CipherAlgEnum cipherAlgEnum, Key key, byte[] sourceData, byte[] iv, boolean encrypt)
+            throws WeGooCipherException {
+        try {
+            Cipher cipher = Cipher.getInstance(cipherAlgEnum.getAlg(), ProviderEngine.getProvider());
+
+            //判断是否需要初始化向量
+            if (cipherAlgEnum.getModeEnum().isNeedIV()) {
+                if (BytesUtil.isEmpty(iv)) {
+                    throw new WeGooCipherException(CipherExceptionMessageEnum.iv_param_empty_err);
+                }
+                cipher.init(encrypt ? Cipher.ENCRYPT_MODE : Cipher.DECRYPT_MODE, key, new IvParameterSpec(iv));
+            } else {
+                cipher.init(encrypt ? Cipher.ENCRYPT_MODE : Cipher.DECRYPT_MODE, key);
+            }
+            return cipher.doFinal(sourceData);
+        } catch (WeGooCipherException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new WeGooCipherException(CipherExceptionMessageEnum.cipher_data_err, e);
         }
-        cipher.init(encrypt ? Cipher.ENCRYPT_MODE : Cipher.DECRYPT_MODE, key, ivParameterSpec);
-      } else {
-        cipher.init(encrypt ? Cipher.ENCRYPT_MODE : Cipher.DECRYPT_MODE, key);
-      }
-      return cipher.doFinal(sourceData);
-    } catch (WeGooCipherException e) {
-      throw e;
-    } catch (Exception e) {
-      throw new WeGooCipherException(CipherExceptionMessageEnum.cipher_data_err, e);
     }
-  }
 
 }
