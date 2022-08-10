68597749ab84368472afbe65b5d649bea6bf1685
zhenwei1108 <zhenwei1108@163.com>
2022-08-10 23:11:49 +0800

    删除无效代码

--- crypto-sdk/src/main/java/com/github/zhenwei/sdk/builder/pkcs7/Pkcs7Builder.java
+++ crypto-sdk/src/main/java/com/github/zhenwei/sdk/builder/pkcs7/Pkcs7Builder.java
@@ -3,13 +3,20 @@ package com.github.zhenwei.sdk.builder.pkcs7;
 import com.github.zhenwei.core.asn1.ASN1Encodable;
 import com.github.zhenwei.core.asn1.x509.Certificate;
 import com.github.zhenwei.core.enums.SignAlgEnum;
+import com.github.zhenwei.core.exception.WeGooEnvelopException;
 
 import java.security.cert.X509CRL;
+import java.security.cert.X509Certificate;
 
 public class Pkcs7Builder extends AbstractPkcs7Builder{
 
     @Override
     ASN1Encodable genSignedData(byte[] data, SignAlgEnum signAlgEnum, byte[] signature, Certificate[] certificates, X509CRL[] crls, boolean isAttach) {
+        return null;
+    }
+
+    @Override
+    ASN1Encodable enveloped(X509Certificate certificate, byte[] data) throws WeGooEnvelopException {
         return null;
     }
 }
