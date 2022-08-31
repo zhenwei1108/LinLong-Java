3e6351e5cb438cbc42276c3a33e61896b03bdaac
zhenwei1108 <zhenwei1108@163.com>
2022-08-31 14:12:13 +0800

    补充fpe描述

--- crypto-core/src/main/java/com/github/zhenwei/core/crypto/fpe/FPEEngine.java
+++ crypto-core/src/main/java/com/github/zhenwei/core/crypto/fpe/FPEEngine.java
@@ -9,6 +9,13 @@ import com.github.zhenwei.core.util.Pack;
 
 /**
  * Base class for format-preserving encryption.
+ * 保留格式加密
+ * FF1 使用十轮变换（安全性相对更高），FF3采用8轮变换（性能相对更高）。
+ * AES——128本身采用十轮变换，AES-192采用十二轮，AES-256采用14轮变换。
+ * 字典表，规定了输出范围。
+ * radix，字典表数量，对于数字，0-9十个数字即可，对于字母则26*2（大小写）。
+ * key，密钥，长度一般为 16，24，32 （貌似描述的是AES密钥）
+ * tweak，相当于第二密钥，防止强度不够。解决因局部加密而导致结果冲突问题。
  */
 public abstract class FPEEngine {
 
