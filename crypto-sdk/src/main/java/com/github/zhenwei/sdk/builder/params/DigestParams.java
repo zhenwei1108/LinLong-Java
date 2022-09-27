8552c9d1fdcd3ba11e2e30ad7954f936f1b3482d
zhangzhenwei <zhenwei1108@163.com>
2022-09-27 23:02:46 +0800

    改用lombok

--- crypto-sdk/src/main/java/com/github/zhenwei/sdk/builder/params/DigestParams.java
+++ crypto-sdk/src/main/java/com/github/zhenwei/sdk/builder/params/DigestParams.java
@@ -4,7 +4,11 @@ import com.github.zhenwei.core.exception.WeGooKeyException;
 import com.github.zhenwei.core.util.encoders.Hex;
 import com.github.zhenwei.sdk.builder.KeyBuilder;
 import java.security.PublicKey;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class DigestParams {
 
   private PublicKey publicKey;
@@ -12,21 +16,6 @@ public class DigestParams {
   // 1234567812345678
   private byte[] userID = Hex.decodeStrict("31323334353637383132333435363738");
 
-  public PublicKey getPublicKey() {
-    return publicKey;
-  }
-
-  public void setPublicKey(PublicKey publicKey) {
-    this.publicKey = publicKey;
-  }
-
-  public byte[] getUserID() {
-    return userID;
-  }
-
-  public void setUserID(byte[] userID) {
-    this.userID = userID;
-  }
 
   public DigestParams(PublicKey publicKey) {
     this.publicKey = publicKey;
