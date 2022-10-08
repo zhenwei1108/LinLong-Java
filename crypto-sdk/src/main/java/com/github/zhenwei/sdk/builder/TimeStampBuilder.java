da82890c074701e20a185306e5cda506081d0a14
zhangzhenwei <zhenwei1108@163.com>
2022-10-08 23:05:38 +0800

    时间戳未完成，不太对

--- crypto-sdk/src/main/java/com/github/zhenwei/sdk/builder/TimeStampBuilder.java
+++ crypto-sdk/src/main/java/com/github/zhenwei/sdk/builder/TimeStampBuilder.java
@@ -1,5 +1,14 @@
 package com.github.zhenwei.sdk.builder;
 
+import com.github.zhenwei.core.asn1.ASN1Boolean;
+import com.github.zhenwei.core.asn1.ASN1Integer;
+import com.github.zhenwei.core.asn1.x509.AlgorithmIdentifier;
+import com.github.zhenwei.core.asn1.x509.KeyPurposeId;
+import com.github.zhenwei.core.enums.DigestAlgEnum;
+import com.github.zhenwei.pkix.util.asn1.tsp.MessageImprint;
+import com.github.zhenwei.pkix.util.asn1.tsp.TimeStampReq;
+import java.math.BigInteger;
+
 /**
  * @description: TimeStamp
  *  时间戳 RFC-3161
@@ -8,5 +17,17 @@ package com.github.zhenwei.sdk.builder;
  * @date: 2022/2/28  10:48 下午
  */
 public class TimeStampBuilder {
+
+  public void generateTssRequest(DigestAlgEnum digestAlgEnum, byte[] digest,String nonce ){
+    AlgorithmIdentifier identifier = new AlgorithmIdentifier(digestAlgEnum.getOid());
+    MessageImprint messageImprint = new MessageImprint(identifier,digest);
+    BigInteger bigIntegerNonce = new BigInteger(nonce);
+    //todo 不太对
+    ASN1Boolean certReq = ASN1Boolean.getInstance(ASN1Boolean.FALSE);
+    TimeStampReq timeStampReq = new TimeStampReq(messageImprint,
+        KeyPurposeId.id_kp_timeStamping.toOID(), new ASN1Integer(bigIntegerNonce), certReq, null);
+
+  }
+
 
 }
